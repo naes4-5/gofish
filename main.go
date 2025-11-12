fc54ab138bb6e61dfe950db0fc27e841cefff9af
Sean E <seanmembley@proton.me>
2025-11-12 16:43:35 -0500

    feat: deck is filled properly

--- main.go
+++ main.go
@@ -25,17 +25,26 @@ type player struct {
 	books int;
 }
 
-func (d deck)draw_random_card() (card, error) {
-	if d.cards_left == 0 {
+func (d *deck)draw_random_card() (card, error) {
+	if d.cards_left <= 0 {
 		return card {}, errors.New("Deck empty: no more cards to draw")
 	}
-	rsuit := suits[rand.IntN(3)]
+	
+	rsuit := suits[rand.IntN(4)]
 	for len(d.cards[rsuit]) < 0 {
 		rsuit = suits[rand.IntN(3)]
 	}
-	rrank := rand.IntN(len(d.cards[rsuit]) - 1)
+
+	var rrank int
+	if len(d.cards[rsuit]) > 1 {
+		rrank = rand.IntN(len(d.cards[rsuit]) - 1)
+	} else {
+		rrank = 0
+	}
+	
 	ret := d.cards[rsuit][rrank]
 	d.cards_left--
+	
 	copy(d.cards[rsuit][rrank:], d.cards[rsuit][rrank+1:])
 	d.cards[rsuit] = d.cards[rsuit][:len(d.cards[rsuit])-1]
 	return ret, nil
@@ -44,10 +53,11 @@ func (d deck)draw_random_card() (card, error) {
 func get_deck() deck {
 	var ret deck
 	ret.cards_left = 52
+	ret.cards = make(map[string][]card)
 	for _, suit := range suits {
 		next := []card{}
 		for i := 0; i < 13; i++ {
-			next = append(next, card {suit: suit, rank: i})
+			next = append(next, card {suit: suit, rank: i + 1})
 		}
 		ret.cards[suit] = next
 	}
@@ -56,9 +66,12 @@ func get_deck() deck {
 
 func main() {
 	deck1 := get_deck()
-	first_card, err := deck1.draw_random_card()
-	if err != nil {
-		log.Fatal(err)
+	for deck1.cards_left > 0 {
+		next_card, err := deck1.draw_random_card()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%d of %s\n", next_card.rank, next_card.suit)
 	}
-	fmt.Printf("%d of %s", first_card.rank, first_card.suit)
 }
+
